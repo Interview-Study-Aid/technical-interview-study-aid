696e1ad7f1c0189d205dae193374cf655c88fa9b
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-10-13 13:48:51 -0700

    added scss and installed scss

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,23 @@
 // Header
 import React from 'react';
 // import Navbar from 'react-bootstrap/Navbar'
-import Navbar from 'react-bootstrap/Navbar';
+// import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav, Form, Button, FormControl} from 'react-bootstrap'
+// import Navbar from 'react-bootstrap/Navbar';
+// import Nav from 'react-bootstrap/Nav'
+// import Form from 'react-bootstrap/Form'
+// import Button from 'react-bootstrap/Button'
+// import FormControl from 'react-bootstrap/FormControl'
 
 const Header = () => {
   return (
     <>
       {/* <h1>Interview Study Aid Header</h1> */}
-      {/* <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Interview Study Aid Header</Navbar.Brand>
-     </Navbar> */}
+    <Navbar bg="dark" variant="dark">
+    <Navbar.Brand>Interview Study Aid Header</Navbar.Brand>
+  
+  </Navbar>
+
     </>
   );
 };
