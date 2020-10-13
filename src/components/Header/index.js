921b7125d34c07c00d9608e5a32d18b18b9f5dbb
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-10-13 13:50:36 -0700

    cleaned header.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,14 @@
 // Header
 import React from 'react';
-// import Navbar from 'react-bootstrap/Navbar'
-// import {Navbar, Nav} from 'react-bootstrap';
-import {Navbar, Nav, Form, Button, FormControl} from 'react-bootstrap'
-// import Navbar from 'react-bootstrap/Navbar';
-// import Nav from 'react-bootstrap/Nav'
-// import Form from 'react-bootstrap/Form'
-// import Button from 'react-bootstrap/Button'
-// import FormControl from 'react-bootstrap/FormControl'
+import Navbar from 'react-bootstrap'
+
 
 const Header = () => {
   return (
     <>
-      {/* <h1>Interview Study Aid Header</h1> */}
-    <Navbar bg="dark" variant="dark">
+  <Navbar bg="dark" variant="dark">
     <Navbar.Brand>Interview Study Aid Header</Navbar.Brand>
-  
   </Navbar>
-
     </>
   );
 };
