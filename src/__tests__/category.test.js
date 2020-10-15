e2f3defbc6359e286c9695bf90096677500dc214
Jennifer Chinzi <jenrose87@gmail.com>
2020-10-15 16:53:41 -0700

    Expanded front end testing

--- src/__tests__/category.test.js
+++ src/__tests__/category.test.js
@@ -1,4 +1,10 @@
-import reducer from '../store/categories';
+import reducer, {selectCategory, getCategories} from '../store/categories';
+import fetchMock from 'fetch-mock';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 it('should have initial state', () => {
   const state = reducer(undefined, {});
@@ -13,4 +19,40 @@ it('should be able to switch categories', () => {
 it('should be able to set initial categories', () => {
   const state = reducer(undefined, {type: 'GET_CATEGORIES', payload: ['JavaScript', '201', 'General']});
   expect(state.categories.length).toEqual(3);
+});
+
+describe('async actions', () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+  
+  it('should return getCategory action', () => {
+    fetchMock.getOnce('/categories', {
+      body: {},
+      headers: {'content-type': 'application/json'}
+    });
+    const expectedActions = [
+      {type: 'GET_CATEGORIES', payload: ['JavaScript', 'ASP.NET', '201', '301', '401', 'General']}
+    ];
+    const store = mockStore({categories: []});
+    return store.dispatch(getCategories()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  it('should return selectCategory action', () => {
+    const category = {category: 'JavaScript', id: '1', questionAnswer:'{}' };
+    fetchMock.getOnce('/categories/JavaScript', {
+      body: category,
+      headers: {'content-type': 'application/json'}
+    });
+    const expectedActions = [
+      {type: 'CHANGE_CATEGORY', payload: []}
+    ];
+    const store = mockStore({activeCategory: {}});
+    return store.dispatch(selectCategory(category)).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
 });
