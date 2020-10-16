9b045fd7bbc86582000133f8be9eaa71c440c2cb
Alexander Whan <whan.alexander@gmail.com>
2020-10-16 00:29:14 -0700

    Removed more comments and unused code

--- src/components/Main/Detail/index.js
+++ src/components/Main/Detail/index.js
@@ -1,11 +1,3 @@
-// Detail View - Modal
-
-// View should display the question in full, as well as:
-//    -Answers (hidden/toggle option to display)
-//    -Notes *if user is logged in* (hidden/toggle option to display)
-//      - Notes should be an editable field and include a save/update button to update persistent data
-//      - Cancel button should revert to last save upon closing (disregarding any edits).
-
 import React, { Children, useState } from 'react';
 import { connect } from 'react-redux';
 import { Modal } from 'react-bootstrap';
@@ -14,7 +6,6 @@ import { closeQuestion } from '../../../store/questions';
 import { getAllNotesForUser } from '../../../store/user';
 import Notes from '../Notes';
 import './detail.scss';
-
 
 function parseQuestion(data) {
   let target = data.questionAnswer;
@@ -67,38 +58,28 @@ const Detail = ({
         className={showHideClassName}
       >
         <Modal.Header>
-          <Modal.Title
-          className="m_title"
-          >{question}</Modal.Title>
+          <Modal.Title className="m_title">{question}</Modal.Title>
         </Modal.Header>
         <section className="modal-main">
-          <Button 
-          onClick={toggleAnswer}
-          className="m_a_button"
-          variant='outline-secondary'
+          <Button
+            onClick={toggleAnswer}
+            className="m_a_button"
+            variant="outline-secondary"
           >
             {!hideAnswer ? 'Hide Answer' : 'View Answer'}
           </Button>
           <br />
-          <p
-          className='answer'
-          >
-          {!hideAnswer && answer}
-          </p>
+          <p className="answer">{!hideAnswer && answer}</p>
           <br />
-          <Button 
-          onClick={toggleNotes}
-          className="m_n_button"
-          variant='outline-info'
+          <Button
+            onClick={toggleNotes}
+            className="m_n_button"
+            variant="outline-info"
           >
             {!hideNotes ? 'Hide Notes' : 'View Notes'}
           </Button>
           <br />
-          <p
-          className="notes"
-          >
-          {!hideNotes && <Notes />}
-          </p>
+          <p className="notes">{!hideNotes && <Notes />}</p>
           <br />
           {/* <button onClick={closeAndReset}>Close</button> */}
         </section>
@@ -112,24 +93,18 @@ const Detail = ({
         className={showHideClassName}
       >
         <Modal.Header>
-          <Modal.Title
-          className="m_title"
-          >{question}</Modal.Title>
+          <Modal.Title className="m_title">{question}</Modal.Title>
         </Modal.Header>
         <section className="modal-main">
-          <Button 
-          onClick={toggleAnswer}
-          variant='outline-secondary'
-          className="m_a_button"
+          <Button
+            onClick={toggleAnswer}
+            variant="outline-secondary"
+            className="m_a_button"
           >
-          {!hideAnswer ? 'Hide Answer' : 'View Answer'}
+            {!hideAnswer ? 'Hide Answer' : 'View Answer'}
           </Button>
           <br />
-          <p
-          className='answer'
-          >
-          {!hideAnswer && answer}
-          </p>
+          <p className="answer">{!hideAnswer && answer}</p>
           <br />
           {/* <button onClick={closeAndReset}>Close</button> */}
         </section>
